6fbe24a68c5a314191c67e2727f8e68a43d78d4e
Juan Camilo Gonzalez <jcgonzalezb@gmail.com>
2022-11-23 17:57:14 -0500

    Listener installed

---
+++ src/test/java/org/Globant/utils/Listener.java
@@ -0,0 +1,17 @@
+package org.Globant.utils;
+
+import org.Globant.reporting.Reporter;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class Listener implements ITestListener {
+
+    @Override
+    public void onTestSuccess(ITestResult result) {
+        Reporter.info("Test: " + result.getName() + " [PASSED]");}
+
+    @Override
+    public void onTestFailure(ITestResult result){
+        Reporter.error("Test: " + result.getName() + " [FAILED]");
+    }
+}
