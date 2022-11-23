11b2d53ee1db386c5a41b1ca4c90fedbac945db7
Juan Camilo Gonzalez <jcgonzalezb@gmail.com>
2022-11-23 17:37:30 -0500

    New class created

---
+++ src/test/java/org/Globant/endpoints/BankTransactions.java
@@ -0,0 +1,113 @@
+package org.Globant.endpoints;
+
+/**
+ * Class relative to the endpoint Bank Transactions
+ */
+public class BankTransactions {
+    private int id;
+    private String name;
+    private String lastName;
+    private int accountNumber;
+    private double amount;
+    private String transactionType;
+    private String email;
+    private boolean active;
+    private String country;
+    private String telephone;
+
+    /**
+     * Constructor class for Bank Transactions Class
+     */
+    public BankTransactions(int id, String name, String lastName, int accountNumber, int amount, String transactionType, String email, boolean active, String country, String telephone) {
+        this.id = id;
+        this.name = name;
+        this.lastName = lastName;
+        this.accountNumber = accountNumber;
+        this.amount = amount;
+        this.transactionType = transactionType;
+        this.email = email;
+        this.active = active;
+        this.country = country;
+        this.telephone = telephone;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public String getTransactionType() {
+        return transactionType;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public void setTransactionType(String transactionType) {
+        this.transactionType = transactionType;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+}
