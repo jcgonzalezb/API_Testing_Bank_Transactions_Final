a437b6e243a3a786204d26fa3119ad005433dde0
Juan Camilo Gonzalez <jcgonzalezb@gmail.com>
2022-11-24 11:40:59 -0500

    deleteTransaction method fixed

--- src/test/java/org/Globant/tests/BaseTest.java
+++ src/test/java/org/Globant/tests/BaseTest.java
@@ -1,24 +1,46 @@
 package org.Globant.tests;
 
 import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
-import org.Globant.endpoints.BankTransactions;
-import org.testng.annotations.BeforeClass;
+import io.restassured.specification.RequestSpecification;
+import org.Globant.endpoints.BankTransaction;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static io.restassured.RestAssured.given;
 
+
 public class BaseTest {
 
-    @BeforeClass
-    public static void setupURL()
-    {
-        // here we set up the default URL and API base path to use throughout the tests
-        RestAssured.baseURI = "https://637d966d9c2635df8f89fa85.mockapi.io";
-        RestAssured.basePath = "/api/v1/bankTransactions/";
+
+    protected List<BankTransaction> getAllTransactions (String endpoint){
+        RestAssured.baseURI = endpoint;
+        RequestSpecification httpRequest = RestAssured.given();
+        Response response = httpRequest.request(Method.GET, "");
+
+        JsonPath jsonPathEvaluator = response.jsonPath();
+        List<BankTransaction> allTransactions = jsonPathEvaluator.getList("", BankTransaction.class);
+        return allTransactions;
     }
+
+    protected void deleteTransaction(String endpoint, BankTransaction bankTransaction){
+        Response response = given()
+                .contentType("application/json")
+                .when()
+                .delete(endpoint + "/" + bankTransaction + bankTransaction.getId());
+    }
+
+    protected void deleteAllTransactions() {
+
+
+
+    }
+
+
+
+
 
     //public List<BankTransactions> getAllTransactions(String endpoint) {
     //    Response response = given().contentType("application/json").when().get(endpoint"); }
